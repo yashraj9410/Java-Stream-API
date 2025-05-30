586996b9cd00b6ae17a6e231cfe669235d0e5645
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2025-05-30 16:11:14 +0530

    Update Main.java

--- Java-Stream/StramBasic/src/main/java/org/example/Main.java
+++ Java-Stream/StramBasic/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 import java.util.*;
 import org.example.streamBeginner.StreamBasic;
+import org.example.streamBeginner.streamStarter.LambaExp;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,5 +10,9 @@ public class Main {
     public static void main(String[] args) {
         StreamBasic st = new StreamBasic();
         st.example1();
+        LambaExp l = new LambaExp();
+        l.lamdaUse1();
+        l.lamdaPredicate();
+        l.lamdaComparator();
     }
 }
